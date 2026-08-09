eb20472a64d0302f082a9eeba3bb530e3da61c34
abhi12012 <agrawalabhishek211@gmail.com>
2026-08-09 14:49:50 +0530

    Add note creation and display functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,62 @@
-
-import Header from "./components/Header";
-import NoteForm from "./components/NoteForm";
-import NotesList from "./components/NotesList";
-
-
-
-
-
+import { useState } from "react";
 
 function App() {
+  const [note, setNote] = useState({
+    title: "",
+    description: ""
+  });
+
+
+  const [notes, setNotes] = useState([]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote({
+      ...note,
+      [name]: value
+    });
+  }
+
+  function handleAddNote() {
+    setNotes([...notes, note]);
+     console.log(notes);
+  }
+
   return (
     <div>
-     <Header
-  title="Advanced Notes App"
-  subtitle="Learn React Step by Step"
->
-  <button>New Note</button>
-</Header>
+      <h1>Advanced Notes App</h1>
+
+      <input
+        name="title"
+        type="text"
+        placeholder="Enter note title"
+        value={note.title}
+        onChange={handleChange}
+      />
+
+      <textarea
+        name="description"
+        placeholder="Enter note description"
+        value={note.description}
+        onChange={handleChange}
+      />
+
+      <button onClick={handleAddNote}>
+        Add Note
+      </button>
 
 
-      <NoteForm />
-      <NotesList />
+      {notes.map((note, index) => (
+  <div key={index}>
+    <h3>{note.title}</h3>
+    <p>{note.description}</p>
+  </div>
+))}
+
+
     </div>
   );
 }
 
 export default App;
-
-
-
