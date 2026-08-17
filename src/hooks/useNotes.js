b3f7a3e493fe741f620da224983f3bbd86ba7746
abhi12012 <agrawalabhishek211@gmail.com>
2026-08-17 16:56:22 +0530

    feat: add error guard

--- src/hooks/useNotes.js
+++ src/hooks/useNotes.js
@@ -6,6 +6,10 @@ import NotesContext from "../context/NotesContext";
 function useNotes() {
   const context = useContext(NotesContext);
 
+  if (!context) {
+  throw new Error("useNotes must be used inside NotesProvider");
+}
+
   return context;
 }
 
